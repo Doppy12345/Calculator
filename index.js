e8f097c9ebded01c2e575849767c0103910c20e8
Daniel Passos <passosdaniel12345@gmail.com>
2020-10-13 00:30:30 -0400

    added comments/descriptions to javascript

--- index.js
+++ index.js
@@ -1,55 +1,37 @@
+// Global variable for the calculator's display element
 let display = document.querySelector('div[data-function=disp]');
+// Global variable for the list of all the operation button elemnts on the calculator
 const operationButtons = Array.from(document.querySelectorAll('.operator'))
 
+// Biggest and smallest displayable number for the calculator without exceeding the maximum number of digits before using scientific notation
 const biggestDisplayableNum = 999999999
 const smallestDisplayableNum = 0.0000001
 
+// The currently saved terms that will be used in an operation along with the operation function
 let storedValues = {firstNum: undefined, secondNum: undefined, operation: null}
+// Global bool that defines if the value in the calculators display will be overwritten 
 let canOverwriteDisp = true
 
-const operate = (chaining) => {
 
-
-   let result = storedValues.operation(storedValues.firstNum, storedValues.secondNum)
-
-    result = formatNumber(result)
-
-   display.textContent = result
-
-   chaining ? storedValues.firstNum = parseFloat(display.textContent): storedValues.firstNum = undefined
-   
-   storedValues.secondNum = undefined
-   storedValues.operation = null
-   unselectOtherOperations(operationButtons)
-   canOverwriteDisp = true
-
-}
 //Basic arithmetic functions
 const divide = (num1, num2 = num1) => num2 ? num1 / num2 : 'Error'
 const multiply = (num1, num2 = num1) => num1 * num2
 const subtract = (num1, num2= num1) => num1 - num2
 const add = (num1, num2 = num1) => num1 + num2
 
-const formatNumber = num => {
-    console.log(num)
-    if(num != 0 && (Math.abs(num) < smallestDisplayableNum || Math.abs(num)  > biggestDisplayableNum)){
-        num = num.toExponential(2)
-    } else if(num.toString().length > 9){
-         num = num.toFixed(8)
-    }
-    return num
-}
-
+//implements the +/- calculator button functionality
 const negate = () => {
     display.textContent *= -1 
     canOverwriteDisp = false
 }
 
+//implements the % calculator button functionality
 const toPercent = () => {
     display.textContent  = formatNumber(parseFloat(display.textContent) / 100)
     canOverwriteDisp = false
 }
 
+//implements the AC calculator button functionality
 const clear = () => {
     if(canOverwriteDisp){
         storedValues.firstNum = undefined
@@ -62,23 +44,55 @@ const clear = () => {
     }
     canOverwriteDisp = true
 }
+//Takes in a number and formats it so that it fits properly in the calculator's display
+const formatNumber = num => {
+    console.log(num)
+    if(num != 0 && (Math.abs(num) < smallestDisplayableNum || Math.abs(num)  > biggestDisplayableNum)){
+        num = num.toExponential(2)
+    } else if(num.toString().length > 9){
+         num = num.toFixed(8)
+    }
+    return num
+}
+// Operate takes in a bool to determine whether it is performing a chain of operations or if it is performing one single operation
+// It then gets the data in storedValues and executes the corresponding arithmetic operation
+const operate = (chaining) => {
 
+    let result = storedValues.operation(storedValues.firstNum, storedValues.secondNum)
+    //Formats the result to fit the calc display
+     result = formatNumber(result)
+    
+    display.textContent = result
+    // If chaining is true it will set the first num to the result of the operation in order to allow for the next operation to chain off of this current one
+    chaining ? storedValues.firstNum = parseFloat(display.textContent): storedValues.firstNum = undefined
+    
+    storedValues.secondNum = undefined
+    storedValues.operation = null
+    unselectOtherOperations(operationButtons)
+    canOverwriteDisp = true
+ 
+ }
 
-const unselectOtherOperations = () => operationButtons.forEach(operator => operator.classList.remove('operator-selected')) 
+ // Function that returns a bool for whether or not  a operation can already be performed
+ const canOperate = () => !(storedValues.firstNum == undefined || canOverwriteDisp)
 
-
+// Function that takes in an imput value and populates the  calculators display with that value
 const populate = (input) =>  {
+    // Checks if the calculator's display only has  zero  and sets canOverwrite display to true so that a n input other than zero must be  given for the display to update
     if(display.textContent === '0'){
         canOverwriteDisp = true
     }
+    //Checks  if the decimal point is the first value and updates the display to reflect that
     if(input == '.' && canOverwriteDisp){
         display.textContent = '0.'
         canOverwriteDisp = false
         return
     }
+    // Checks to see if the display already contains a decimal  and if it does it doesn't add  another one
     else if(input=='.' && display.textContent.includes('.')){
         return
     }
+    // Checks to see if the display is full while still allowiing for an operation to be selected and a second value to be inputted
     if(display.textContent.length <= 8 && !canOverwriteDisp){
         display.textContent += input 
     }else if(canOverwriteDisp){
@@ -87,36 +101,46 @@ const populate = (input) =>  {
     }
 }
 
-const canOperate = () => !(storedValues.firstNum == undefined || canOverwriteDisp)
+// Removes the operated selected CSS style from all the operators 
+const unselectOtherOperations = () => operationButtons.forEach(operator => operator.classList.remove('operator-selected')) 
 
-
-
+// Sets up the calculator by creating all necessary event listeners and implementing their functionality
 function setUpCalculator(){
 
+    // Adds an event listen for all keyboard keys with a corresponding calculator input
+    // Excludes AC, and +/- calculator buttons
     window.addEventListener('keypress', e => {
+        // The pressed key is stored as the input
         const input = document.querySelector(`[data-key="${e.key}"]`)
         
+        //  If the input is a num-input key populate the corresponding number into the display
         if(input.className == 'num-input'){
         input.classList.add('num-input-pressed')
         populate(input.textContent)
         }
 
+        // If the input is an operator  perform or select the operation depending on if an operation can already be performed
         else if(input.className == 'operator' ){
+            // Performs a specific action depending on which operator was pressed
             switch(input.getAttribute('data-function')){
                 case 'multiply':
+                    // Check to see if an operation can already be performed
                         if(!canOperate()){
                             storedValues.firstNum = parseFloat(display.textContent) 
                             canOverwriteDisp = true
-                        }else{
+                        }
+                        // If an operation can already be performed, perform that operation in chaining mode 
+                        else{
                            storedValues.secondNum = parseFloat(display.textContent)
                            operate(true)
                         }
-    
+                        // Select  this operation and unselect all other operations
                         storedValues.operation = multiply
                         input.classList.add('operator-clicked')
                         unselectOtherOperations()
                         input.classList.add('operator-selected')
                     break
+                // Do the same for each other operation as in multiply    
                 case 'divide':
                         if(!canOperate()){
                             storedValues.firstNum = parseFloat(display.textContent) 
@@ -169,6 +193,7 @@ function setUpCalculator(){
         }
     })
 
+    // Gets a list of all html elements corresponding to a keyboard key and removes the completed animation class from each of  their classLists
     const keys = Array.from(document.querySelectorAll('[name="key"]'))
     keys.forEach(key => {
         key.addEventListener('animationend', e =>{
@@ -177,8 +202,8 @@ function setUpCalculator(){
         })
     })
     
+    // Gets a list of all num-input keys and adds a onclick event listener to each of them similar to the on press event listener
     let numButtons = Array.from(document.querySelectorAll('.num-input'))
-    
     numButtons.forEach(button => {
         button.addEventListener('click', e => {
             e.target.classList.add('num-input-clicked')
@@ -204,7 +229,7 @@ function setUpCalculator(){
             toPercent()
         })
     
-
+    // adds an on click event listener to all the operation buttons on the calculator similar to their corresponding  keypress event listener functionality
     operationButtons.forEach(button => {
         switch(button.getAttribute('data-function')){
             case 'multiply':
@@ -286,5 +311,5 @@ function setUpCalculator(){
 }
 
 
-
+// Sets up the calculator 
 setUpCalculator();
